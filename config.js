eb6d174485576ff40d4bc9aeec1dff04a6147da4
GODBOIIIIII <panchamprabhune22@gmail.com>
2025-02-13 23:36:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/drufmjoga/video/upload/v1739469581/gd2mgp6hfiscft5awrt6.ogg", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                      // Volume level (0.0 to 1.0)
+        volume: 1.0                     // Volume level (0.0 to 1.0)
     }
 };
 
