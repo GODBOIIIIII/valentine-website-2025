e3d51e42b32fa124db6c6cefd53b127b7f57242e
GODBOIIIIII <panchamprabhune22@gmail.com>
2025-02-13 23:32:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,10 +78,10 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.youtube.com/watch?v=q4GJVOMjCC4", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/drufmjoga/video/upload/v1739469581/gd2mgp6hfiscft5awrt6.ogg", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 5.0                      // Volume level (0.0 to 10.0)
+        volume: 0.5                      // Volume level (0.0 to 1.0)
     }
 };
 
